c65c7b9659f647ed6e4dac86c483c7456d29dd35
1smaa <77068062+1smaa@users.noreply.github.com>
2021-11-12 20:51:43 +0100

    Add files via upload

---
+++ pymultisort/pymultisort.py
@@ -0,0 +1,67 @@
+def selectionSort(array):
+    """Apply selection sort to an array. Returns a sorted array."""
+    for i in range(len(array)):
+        min_index = i
+        for j in range(i+1, len(array)):
+            if array[min_index] > array[j]:
+                min_index = j
+        array[i], array[min_index] = array[min_index], array[i]
+    return array
+
+
+def mergeSort(array):
+    """Apply merge sort to an array. Returns a sorted array."""
+    if len(array) > 1:
+        mid = len(array) // 2
+        left = array[:mid]
+        right = array[mid:]
+        mergeSort(left)
+        mergeSort(right)
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left) and j < len(right):
+            if left[i] <= right[j]:
+                array[k] = left[i]
+                i += 1
+            else:
+                array[k] = right[j]
+                j += 1
+            k += 1
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+    return array
+
+
+def quickSort(array):
+    """Apply quick sort to an array. Returns a sorted array."""
+    array.sort()
+    return array
+
+
+def insertionSort(array):
+    """Apply insertion sort to an array. Returns a sorted array."""
+    for i in range(1, len(array)):
+        key = array[i]
+        j = i-1
+        while j >= 0 and key < array[j]:
+            array[j+1] = array[j]
+            j -= 1
+        array[j+1] = key
+    return array
+
+
+def bubbleSort(array):
+    """Apply bubble sort to an array. Returns a sorted array."""
+    n = len(array)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(name="PyMultiSort",
+      author="Ismaele Lorenzon",
+      packages=["pymultisort"],
+      version="0.0.1",
+      author_email="lorenzonismaele@gmail.com",
+      description="A Python package that brings the most important sorting algorithms together.",
+      license="MIT License")
